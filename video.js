a44d4bc382023f11ebc8ce04d564c967ced4dc71
invtr07 <95737990+invtr07@users.noreply.github.com>
2022-06-10 17:56:54 +0600

    finally made div to appear and blur too

--- video.js
+++ video.js
@@ -1,11 +1,15 @@
 let videoJs = document.getElementById("videoJs");
-               let btn= document.getElementById("PlayPause");
+let btn= document.getElementById("PlayPause");
                
 function playVideo(){
      if(videoJs.paused){
           videoJs.play();
           videoJs.style="filter: none";
-          btn.innerHTML='<div><i class="fa fa-pause-circle fa-5x"></i></div>';             
+          btn.innerHTML='<div><i class="fa fa-pause-circle fa-5x"></i></div>';  
+          //browser should delete this div if user will rewatch the video
+          if(endWish=document.getElementsByClassName("luck")){
+               videoJs.removeChild(endWish);
+          }           
      }
      else{
           videoJs.pause();
@@ -13,8 +17,20 @@ function playVideo(){
      }
 }
 
-     videoJs.onended= ()=> {//when video stops "it will be blurred"           
+     videoJs.onended= ()=> {           
           videoJs.style="filter: blur(40px)";
-          btn.innerHTML='<div><i class="fa fa-pause-circle fa-5x"></i></div>'
-          videoJs.innerHTML='<p style="color: white">Good Luck on the mastery of JS<p>'
-     };
+          btn.innerHTML='<div><i class="fa fa-play-circle fa-5x"></i></div>'
+          // let container= document.getElementById("container");
+          let wishMessage = document.createElement("div");
+          wishMessage.className='luck';
+          wishMessage.innerHTML='Good luck man with tackling JS!';
+          
+          videoJs.appendChild(wishMessage);
+      };
+
+      
+
+      
+
+
+  
